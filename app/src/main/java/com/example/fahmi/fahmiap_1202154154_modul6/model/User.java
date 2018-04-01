7b6886a84a6d9f971d6870790208572fc2646006
Fahmi Ananda <fahmianandap@gmail.com>
2018-04-01 22:59:17 +0700

    M6

---
+++ app/src/main/java/com/example/fahmi/fahmiap_1202154154_modul6/model/User.java
@@ -0,0 +1,29 @@
+package com.example.fahmi.fahmiap_1202154154_modul6.model;
+
+public class User {
+
+    String userId;
+    String username;
+    String email;
+
+    public User() {
+    }
+
+    public User(String userId, String username, String email) {
+        this.userId = userId;
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
